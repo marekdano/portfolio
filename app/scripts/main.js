4869e8ae10ec8ab1dd4554519a940eed70d855fe
Marek Dano <mk.dano@gmail.com>
2016-03-04 00:38:58 +0000

    Add typedjs plugin and interaction with the text to the website.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
 console.log('\'Allo \'Allo!');
+
+$(() => {
+  $(".element").typed({
+    strings: ["Web developer ^1000 at the night", "Tester at the day"], 
+    
+  });
+
+  $(".typed-sentences").typed({
+    stringsElement: $('.typed-strings'),
+    showCursor: false,
+    typeSpeed: 100,
+    callback: function(){
+      $('.typed-sentences').hide();
+      $('h4').fadeIn('slow');
+    }
+  });
+});
