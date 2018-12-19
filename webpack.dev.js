3486d59dcdef25079a37801642ee05a5fdfe125b
Marek Dano <mk.dano@gmail.com>
2018-12-19 22:01:12 +0000

    Add webpack.dev.js config.

---
+++ webpack.dev.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+
+  // This option controls if and how source maps are generated.
+  // https://webpack.js.org/configuration/devtool/
+  devtool: 'cheap-module-eval-source-map' ,
+
+  // https://webpack.js.org/concepts/entry-points/#multi-page-application
+  entry: {
+    index: './src/page-index/main.js',
+  },
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 9000
+  },
+
+  // https://webpack.js.org/concepts/loaders/
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // On development we want to see where the file is coming from, hence we preserve the [path]
+              name: '[path][name].[ext]?hash=[hash:20]',
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ],
+  },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      template: './src/page-index/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    }),
+  ]
+};
